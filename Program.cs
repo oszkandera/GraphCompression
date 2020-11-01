bf37bc53b5f0dff849f6ad7994566e112ff38db6
Ondra Szkandera <szkanderao@gmail.com>
2020-11-01 15:38:32 +0100

    master - Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using GraphProcessor.Core.GraphLoader;
+using System;
+
+namespace GraphCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
