1a612ae90bd2a7a46523baae67f0129a01f02f2c
Ondra Szkandera <szkanderao@gmail.com>
2021-04-07 19:41:16 +0200

    Testing - GrapCompressor - Not MappedReferences handle

--- Test/UnitTest/GraphCompressorTest.cs
+++ Test/UnitTest/GraphCompressorTest.cs
@@ -131,6 +131,78 @@ namespace Test.UnitTest
             CheckExpectedAgainstResult(expectedResult, result);
         }
 
+        [Fact]
+        public void ShouldHandleNotMappedReferences()
+        {
+            var compressParameters = new CompressParameters { MaxReferenceListSize = 5 };
+            var similarNodeList = new List<SimilarNode>()
+            {
+                new SimilarNode
+                {
+                    Node1 = 1,
+                    Node2 = 2,
+                    Neighbors1 = new SortedList<int, int> { { 1, 30 }, { 2, 40 } },
+                    Neighbors2 = new SortedList<int, int> { { 1, 30 } }
+                },
+                new SimilarNode
+                {
+                    Node1 = 3,
+                    Node2 = 4,
+                    Neighbors1 = new SortedList<int, int> { { 1, 70 }, { 2, 80 } },
+                    Neighbors2 = new SortedList<int, int> { }
+                },
+                new SimilarNode
+                {
+                    Node1 = 5,
+                    Node2 = 6,
+                    Neighbors1 = new SortedList<int, int> { { 1, 75 }, { 2, 85 } },
+                    Neighbors2 = new SortedList<int, int> { { 1, 85 }, { 2, 90 } }
+                },
+                new SimilarNode
+                {
+                    Node1 = 7,
+                    Node2 = 8,
+                    Neighbors1 = new SortedList<int, int> { { 1, 90 }, { 2, 96 } },
+                    Neighbors2 = new SortedList<int, int> { { 1, 85 }, { 2, 88 } }
+                },
+            };
+
+            var expectedResult = new CompressedGraph();
+            expectedResult.AddNode(new CompressedNode
+            {
+                Id = 1,
+                ReferenceId = 2,
+                ReferenceList = new BitArray(new bool[] { true }),
+                ExtraNodes = new SortedSet<int>() { 40 },
+            });
+            expectedResult.AddNode(new CompressedNode
+            {
+                Id = 3,
+                ReferenceId = 4,
+                ReferenceList = new BitArray(0),
+                ExtraNodes = new SortedSet<int>() { 70, 80 },
+            });
+
+            expectedResult.AddNode(new CompressedNode
+            {
+                Id = 5,
+                ReferenceId = 6,
+                ReferenceList = new BitArray(new bool[] { true, false }),
+                ExtraNodes = new SortedSet<int> { 75 },
+            });
+
+            expectedResult.AddNode(new CompressedNode
+            {
+                Id = 7,
+                ReferenceId = 8,
+                ReferenceList = new BitArray(new bool[] { false, false }),
+                ExtraNodes = new SortedSet<int> { 90, 96 },
+            });
+
+            var result = ExecuteTest(similarNodeList, compressParameters);
+            CheckExpectedAgainstResult(expectedResult, result);
+        }
+
         private CompressedGraph ExecuteTest(List<SimilarNode> similarNodeList, CompressParameters compressParameters)
         {
             var sortingAlgorithm = GetMockedSortingAlgorithm().Object;
